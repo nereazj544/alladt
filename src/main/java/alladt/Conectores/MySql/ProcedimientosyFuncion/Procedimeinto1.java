d903be2f8aef44ece0308e13c921f32b056ef2b8
nereazj544 <nzjvirtual@gmail.com>
2024-05-31 01:12:43 +0200

    P y F

--- src/main/java/alladt/Conectores/MySql/ProcedimientosyFuncion/Procedimeinto1.java
+++ src/main/java/alladt/Conectores/MySql/ProcedimientosyFuncion/Procedimeinto1.java
@@ -1,5 +1,6 @@
 package alladt.Conectores.MySql.ProcedimientosyFuncion;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,7 +11,9 @@ public class Procedimeinto1 {
             Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/napoles?useSSL=true&serverTimezone=UTC", "haku", "123");
             String procedimiento = "{call pro1}";
 
-
+            CallableStatement cs = conexion.prepareCall(procedimiento);
+            cs.execute();
+            System.out.println("> Se ha creado la tabla");
                 
         }catch (SQLException e) {
             System.out.println("> El sistema no a encontrado la base de datos, la tabla o el campo.\n O campo repetido");
