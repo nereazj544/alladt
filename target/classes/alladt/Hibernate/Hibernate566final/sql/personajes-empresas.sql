7c02f5fea429780e7f4a485f202dcc6c7970e19c
nereazj544 <nzjvirtual@gmail.com>
2024-06-02 16:35:13 +0200

    .--.

---
+++ target/classes/alladt/Hibernate/Hibernate566final/sql/personajes-empresas.sql
@@ -0,0 +1,28 @@
+--! TABLAS
+create table empresa(
+    id int auto_increment PRIMARY KEY,
+    nombre VARCHAR(255) NOT Null
+);
+
+create table personajes(
+    id int auto_increment PRIMARY KEY,
+    nombre VARCHAR(255) NOT Null,
+    juego  VARCHAR(255) NOT Null,
+    empresa_id int,
+    foreign key (empresa_id) references empresa(id)
+);
+
+--! DATOS INSETAR
+insert INTO empresa (nombre) values ('Hoyoverse'), ('Kuro Game');
+
+
+insert INTO personajes (nombre, juego, empresa_id) values
+('Bennett', 'Genshin Impact', 1),
+('Kazuha', 'Genshin Impact', 1),
+('Arlecchino', 'Genshin Impact', 1),
+('Jing Yan', 'Honkai Star Rail', 1),
+('Kafka', 'Honkai Star Rail', 1),
+('Gepard', 'Honkai Star Rail', 1),
+('Jiyan', 'Wuthering Waves', 2),
+('Calcharo', 'Wuthering Waves', 2),
+('Lingyang', 'Wuthering Waves', 2);
