a7004a530b0c362b6f9d480958891d251d7a8552
nereazj544 <nzjvirtual@gmail.com>
2024-06-03 23:42:54 +0200

    MongoTXT - funciona :0

--- src/main/java/alladt/MongoDB/MongoDB_TXT.java
+++ src/main/java/alladt/MongoDB/MongoDB_TXT.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoDB_TXT {
@@ -33,8 +36,44 @@ public class MongoDB_TXT {
 
 
             while ((linea = bf.readLine()) != null) {
-                
+                String[] p = linea.split(": ");
+                if(p.length < 2) continue; //! Salta lineas mal formadas
+                String k = p[0].trim();
+                String v = p[1].trim();
+
+                switch (k) {
+                    case "nombre":
+                        nombre = v;
+                        break;
+                    case "posicion":
+                        posicion = v;
+                        break;
+                    case "elemento":
+                        elemento = v;
+                        break;
+                    case "arma":
+                        arma = v;
+                        break;
+                    case "rareza":
+                        rareza = Integer.parseInt(v);
+                        break;
+                }
+
             }
+
+            bf.close();
+
+            Document doc = new Document();
+            doc.put("nombre", nombre);
+            doc.put("rareza", rareza);
+            doc.put("posicion", posicion);
+            doc.put("elemento", elemento);
+            doc.put("arma", arma);
+
+            MongoCollection<Document> collection = db.getCollection("Calcharo <3");
+
+            collection.insertOne(doc);
+            System.out.println("> El sistema a introducido correctamente los elementos del txt.");
         } catch (Exception e) {
             System.out.println("> El sistema no ha podido seguir las indicaciones dadas.");
             System.out.println("> El sitema proporciona el error: " + e.getMessage());
