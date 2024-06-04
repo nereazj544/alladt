59742636b28bdf5aab1bcbedd5e894e752fe8d8b
nereazj544 <nzjvirtual@gmail.com>
2024-06-04 11:48:00 +0200

    mongo

--- src/main/java/alladt/MongoDB/Consultas/Consultas.java
+++ src/main/java/alladt/MongoDB/Consultas/Consultas.java
@@ -11,8 +11,11 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+
+
 
 public class Consultas {
     public static void main(String[] args) {
@@ -38,7 +41,7 @@ public class Consultas {
                 Insertardatos();
                 break;
             case "Consulta":
-                Consulta();
+                Consulta(sc);
                 break;
             default:
                 break;
@@ -86,7 +89,91 @@ public class Consultas {
         }
     }
 
-    private static void Consulta() {
+    private static void Consulta(Scanner sc) {
+        sc.nextLine(); //** Limpia el buffer
+        
+        System.out.println("> El sistema requiere que selecione una opcion: nombre, arma, rareza o elemento");
+        String r = sc.nextLine();
 
+        switch (r) {
+            case "nombre":
+                consultaNombre(sc);
+                break;
+            case "arma":
+                consultaArma(sc);
+                break;
+            case "rareza":
+                consultaRareza(sc);
+                break;
+            case "elemento":
+                consultaElemento(sc);
+                break;
+        
+            default:
+            System.out.println("> El sistema no ha detectado una opcion valida. Pruebe de nuevo.");
+                break;
+        }
+    }
+
+    private static void consultaNombre(Scanner sc) {
+        sc.nextLine();
+        String consulta = "Nombre";
+        System.out.println("> El sistema ha detectado la orden de la consulta: " + consulta);
+        try {
+            
+            
+            System.out.println("> El sistema ha extablecido conexion con MongoDB: ");
+            MongoClient m = new MongoClient();
+            MongoDatabase db = m.getDatabase("WutheringWuvas");
+            MongoCollection<Document> c = db.getCollection("Consultas");
+            
+            System.out.println("\n");
+            
+            System.out.println("> El sistema requiere que se introduca un: " + consulta);
+            String nombre = sc.nextLine();
+            
+            Document filtro = new Document("nombre", nombre);
+            FindIterable<Document> busqueda = c.find(filtro);
+
+            System.out.println("> El sistema ha en");
+            for (Document document : busqueda) {
+                System.out.println(document.toJson());
+            }
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+    
+
+    private static void consultaArma(Scanner sc) {
+        sc.nextLine();
+        String consulta = "Arma";
+        System.out.println("> El sistema ha detectado la orden de la consulta: " + consulta);
+        
+        System.out.println("> El sistema requiere que se introduca un: " + consulta);
+        String con = sc.nextLine();
+        
+        
+    }
+    
+    private static void consultaRareza(Scanner sc) {
+        sc.nextLine();
+        String consulta = "Rareza";
+        System.out.println("> El sistema ha detectado la orden de la consulta: " + consulta);
+        
+        System.out.println("> El sistema requiere que se introduca una: " + consulta + " (4 o 5)");
+        int con = sc.nextInt();
+        
+    }
+    
+    private static void consultaElemento(Scanner sc) {
+        sc.nextLine();
+        String consulta = "Elemento";
+        System.out.println("> El sistema ha detectado la orden de la consulta: " + consulta);
+        
+        System.out.println("> El sistema requiere que se introduca un: " + consulta);
+        String con = sc.nextLine();
     }
 }
